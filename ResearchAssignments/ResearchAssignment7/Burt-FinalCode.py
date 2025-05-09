1c169da1d36ad7772b99558ebb3d44707ec07f25
Christian Burt <caburt@Christians-MacBook-Pro.local>
2025-05-08 17:03:47 -0700

    Final Project submission

---
+++ ResearchAssignments/ResearchAssignment7/Burt-FinalCode.py
@@ -0,0 +1,313 @@
+import numpy as np
+from numpy.typing import NDArray
+import astropy.units as u
+import matplotlib
+import matplotlib.pyplot as plt
+from ReadFile import Read
+from Lab7 import RotateFrame
+from CenterOfMass2 import CenterOfMass
+from Snapshots import snapshotsCompile
+import os
+from collections import deque
+
+def majorAnnuli(
+        x:NDArray[np.float64],
+        y:NDArray[np.float64],
+        Nmin:int,
+        Nmax:int,
+        delta:float = 0.15
+) -> NDArray[np.int_]:
+    """
+    This function finds the mass of each overlapping annulus
+
+    Arguments:
+        x (kpc): adjusted sorted x positions of the stars
+        y (kpc): adjusted sorted y positions of the stars
+        Nmin: The minimum number of stars per annulus
+        Nmax: The maximum number of stars per annulus
+        delta: A parameter that determines where to divide the annulus
+               r[last-1]/r[first] < 10**delta
+    Returns:
+        binIndices: contains the minimum and maximum radius of each annulus
+    """
+
+    binIndices:collections.deque = deque()
+    # This is an optimized list I am using as a compromise, since the
+    # numpy array restriction is difficult to implement for a list
+    # that does not have a known initial size.
+
+    
+    r:NDArray[np.float64] = np.linalg.norm(np.vstack((x,y)),axis=0)
+    i:int = 0
+    N:int = r.size
+
+    while i < N:
+        first:int = i
+        last:int = min((N,i+Nmin))
+
+        while last < N and (last-first) < Nmax and (r[last-1]/r[first] < 10**delta):
+            last += 1
+        binIndices.append((first,last))
+        # I have approval to use this append
+        # Note that it is appending a deque, not a list
+        i = last
+
+    binIndicesArr = np.array(binIndices,dtype=int)
+    return binIndicesArr
+
+def overlapAnnuli(
+        binIndices:NDArray[np.int_],
+        r:NDArray[np.float64]
+) -> NDArray[np.int_]:
+    """
+    This function finds the mass of each overlapping annulus
+    
+    Arguments:
+        binIndices: contains the minimum and maximum radius of each annulus
+        r (kpc): sorted radii of the stars
+    Returns:
+        overlapIndices: binIndices with the added overlap regions between them.
+    """
+
+    overlapIndices:NDArray[np.int_] = np.zeros((2*binIndices.shape[0]-1,2), dtype=np.int_)
+    overlapIndices[::2] = binIndices
+    overlapIndices[1:-1:2] = (binIndices[:-1] + binIndices[1:])//2
+    return overlapIndices
+
+def barStrength(
+        x:NDArray[np.float64],
+        y:NDArray[np.float64],
+        m:NDArray[np.float64],
+        annuli:NDArray[np.int_],
+        nphi:int,
+        mMax:int
+) -> tuple[NDArray[np.float64],float]:
+    """
+    This function finds the mass of each overlapping annulus and performs a Fourier
+    analysis to compute the bar strength as the different in the root-mean-squared
+    of the even and odd modes
+
+    Arguments:
+        x (kpc): adjusted sorted x positions of the stars
+        y (kpc): adjusted sorted y positions of the stars
+        m (10^10 Msun): sorted masses of the stars
+        annuli: contains the minimum and maximum radius of each annulus
+                This should be taken from the overlapping annuli
+        nphi: number of desired azimuthal bins
+        mMax: largest mode for the Fourier analysis
+    
+    Returns:
+        strengthsArr: an array of S values for each overlapping bin.
+        galaxyStrength: the maximum S value (used to quantify the overall bar strength).
+    """
+    r = np.linalg.norm(np.vstack((x,y)),axis=0)
+    phi = np.mod(np.arctan2(y,x),2*np.pi)
+    
+    mModes:NDArray[np.float64] = np.arange(1,mMax+1)
+    strengthsArr:NDArray[np.float64] = np.zeros(np.shape(annuli)[0])
+
+    mAnnuli:NDArray[np.float64] = np.zeros(annuli.shape[0])
+    for i,annulus in enumerate(annuli):
+        first,last = annulus
+        mAnnulus:NDArray[np.float64] = m[first:last]
+        phiAnnulus:NDArray[np.float64] = phi[first:last]
+
+        mAnnuli[i] = np.sum(mAnnulus)
+
+        MProf:NDArray[np.float64]
+        MProf,_ = np.histogram(phiAnnulus,bins=nphi,range=(0,2*np.pi),weights=mAnnulus)
+
+        Sigma:float
+        mAmp:NDArray[np.float64]
+        mPhase:NDArray[np.float64]
+        Sigma,mAmp,mPhase = fourier(MProf, mModes)
+     
+        oddAmp:NDArray[np.float64] = mAmp[0::2]
+        evenAmp:NDArray[np.float64] = mAmp[1::2]
+        annulusStrength:float = np.sqrt(np.sum(evenAmp**2))
+        strengthsArr[i] = annulusStrength
+    galaxyStrength:float = np.sum(strengthsArr*mAnnuli)/np.sum(mAnnuli)
+    
+    return strengthsArr,galaxyStrength
+
+def fourier(
+        MProf:NDArray[np.float64],
+        mModes:NDArray[np.float64]
+) -> tuple[float,NDArray[np.float64],NDArray[np.float64]]:
+    """
+    This function runs a simple Fourier analysis using a uniform (tophat) window.
+
+    Arguments:
+        MProf (10^10 Msun): the mass profile of the annuli
+        mModes: all modes necessary for analysis
+        mMax: largest mode for the Fourier analysis
+    
+    Returns:
+        Sigma (10^10 Msun): the mean value of the profile
+        mAmp: the amplitudes of mode m
+        mPhase: the phases of mode m
+    """
+    
+    n:int = MProf.size
+    Sigma:float = np.mean(MProf)
+    mAmp:NDArray[np.float64] = np.zeros(mModes.size)
+    mPhase:NDArray[np.float64] = np.zeros(mModes.size)
+    theta:NDArray[np.float64] = np.linspace(0,2*np.pi,n,endpoint=False)
+    for i,m in enumerate(mModes):
+        cn:float = np.sum(MProf*np.exp(-1j*m*theta))
+        mAmp[i] = np.abs(cn)
+        mPhase[i] = np.angle(cn)
+    return Sigma,mAmp,mPhase
+
+
+def plotBarStrengthPerAnnulus(
+    strengthsArr:NDArray[np.float64],
+    annuliIndices:NDArray[np.int_],
+    r:NDArray[np.float64],
+    snapshot:int,
+    galaxy:str,
+    fig=None,
+    ax=None
+) -> None:
+    # All the inner edges of the annuli
+    edges:NDArray[np.int_] = r[annuliIndices[:,0]]
+
+    # Places the most outer edge at the end
+    edges = np.concatenate((r[annuliIndices[:,0]],[r[annuliIndices[-1,1]-1]]))
+
+    midpoints:NDArray[np.float64] = 0.5*(edges[:-1]+edges[1:])
+    widths:NDArray[np.float64] = edges[1:] - edges[:-1]
+
+    if fig == None or ax == None:
+        fig,ax = plt.subplots()
+    ax.bar(midpoints, strengthsArr, width=widths, align='center',
+           edgecolor='k', alpha=0.7)
+    
+    ax.text(0.92,0.82,galaxy,transform=ax.transAxes,ha="right",va="center",size=12)
+    ax.text(0.92,0.68,f'{snapshot/70:.2f} Gyr',transform=ax.transAxes,ha="right",va="center",size=12)
+    ax.tick_params(labelsize=14)
+
+    ax.set(
+        xlim=(-5,79.7),
+        ylim=(0,0.7)
+        )
+
+    #fig.savefig(f"{galaxy}_{snapshot:03d}_bar_histogram.pdf")
+
+def plotGalaxyStrengthPerSnapshot(
+        strengths:NDArray[np.float64],
+        galaxy:str,
+        fig=None,
+        ax=None
+)-> None:
+
+    if fig == None or ax == None:
+        fig,ax = plt.subplots()
+    ax.plot(strengths[:,0]/70,strengths[:,1],c='r')
+    ax.text(0.15,0.82,galaxy,transform=ax.transAxes,ha="right",va="center",size=22)
+    ax.tick_params(labelsize=20)
+
+    # ax.set(title=f"{galaxy} Strength per snapshot",xlabel='Snapshot',ylabel='Bar Strength')
+    # fig.savefig(f"{galaxy}_bar_strength.pdf")
+
+def main() -> None:
+    foci:tuple[int,...] = (275,410,470) # 3.929 Gyr, # 5.857 Gyr, # 6.714 Gyr
+
+    first:int = 0
+    last:int = 801
+
+    # generates majors from the halfway points between foci
+    fociArr:NDArray[np.int_] = np.sort(np.array(foci))
+    halfways:NDArray[np.int_] = (fociArr[:-1]+fociArr[1:])//2
+
+    majors:NDArray[np.int_] = np.array([first,*tuple(halfways),last-1])
+    snapshots:tuple[int,...] = snapshotsCompile(4,foci=foci)
+
+    # Parameters
+    Nmin:int = 8000 # minimum number of particles per major annulus
+    Nmax:int = 64000 # maximum number of particles per major annulus
+    nphi:int = 360 # angle iterations to cover a circle
+    mMax:int = 2 # For max resoluion, this would be the Nyquist frequency.
+                 # However, this makes the data noisy
+    galaxies:tuple[str,str] = ('MW','M31')
+    strengths:NDArray[np.float64] = np.zeros((len(snapshots),2))
+
+    fig1,ax1 = plt.subplots(len(galaxies),1,sharex=True,sharey=True,figsize=(8,6),gridspec_kw={'hspace':0,'wspace':0})
+
+    fig2,ax2 = plt.subplots(majors.size,len(galaxies),sharex=True,sharey=True,figsize=(8,6),gridspec_kw={'hspace':0,'wspace':0})
+
+    for j,galaxy in enumerate(galaxies):
+        maji:int = 0
+        for i,snapshot in enumerate(snapshots):
+            ScriptDir = os.path.dirname(os.path.abspath(__file__))
+            filename = os.path.join(ScriptDir,galaxy,f"{galaxy}_{snapshot:03d}.txt")
+            time:u.Quantity
+            total:int
+            data:NDArray[np.float64]
+            time,total,data = Read(filename)
+
+            com:CenterOfMass2.CenterOfMass = CenterOfMass(filename,2)
+            comMass:NDArray[np.float64] = com.m
+
+            COMP:u.Quantity = com.COM_P(0.1)
+            COMV:u.Quantity = com.COM_V(COMP[0],COMP[1],COMP[2])
+
+            # Determine positions of disk particles relative to COM
+            xcom:NDArray[np.float64] = com.x - COMP[0].value
+            ycom:NDArray[np.float64] = com.y - COMP[1].value
+            zcom:NDArray[np.float64] = com.z - COMP[2].value
+            rcom:NDArray[np.float64] = np.vstack((xcom,ycom,zcom)).T
+
+            vxcom:NDArray[np.float64] = com.vx - COMV[0].value
+            vycom:NDArray[np.float64] = com.vy - COMV[1].value
+            vzcom:NDArray[np.float64] = com.vz - COMV[2].value
+            vcom:NDArray[np.float64] = np.vstack((vxcom,vycom,vzcom)).T
+
+            rComps,vComps = RotateFrame(rcom,vcom)
+            rUnsorted = np.linalg.norm(rComps, axis=1)
+            ri:int = np.argsort(rUnsorted)
+            r:NDArray[np.float64] = rUnsorted[ri]
+
+            mass:NDArray[np.float64] = comMass[ri]
+            x:NDArray[np.float64] = rComps[ri,0]
+            y:NDArray[np.float64] = rComps[ri,1]
+            z:NDArray[np.float64] = rComps[ri,2]
+
+            # NOTE: The velocity is currently unused
+            vx:NDArray[np.float64] = vComps[ri,0]
+            vy:NDArray[np.float64] = vComps[ri,1]
+            vz:NDArray[np.float64] = vComps[ri,2]
+            v:NDArray[np.float64] = np.linalg.norm(np.vstack((vx,vy,vz)),axis=0)
+
+
+            majorAnnuliIndices:NDArray[np.int_] = majorAnnuli(x,y,Nmin,Nmax)
+            overlappingAnnuliIndices:NDArray[np.int_] = overlapAnnuli(majorAnnuliIndices,r)
+            strengthsArr,galaxyStrength = barStrength(x,y,mass,overlappingAnnuliIndices,nphi,mMax)
+
+            strengths[i] = np.array([snapshot,galaxyStrength])
+            if maji < majors.size and snapshot >= majors[maji]:
+                plotBarStrengthPerAnnulus(
+                    strengthsArr,
+                    overlappingAnnuliIndices,
+                    r,
+                    snapshot,
+                    galaxy,
+                    fig=fig2,
+                    ax=ax2[maji,j]
+                )
+
+                maji += 1
+        plotGalaxyStrengthPerSnapshot(strengths,galaxy,fig1,ax1[j])
+
+
+    fig1.supxlabel('time [Gyr]', fontsize=22)
+    fig1.supylabel('Bar Strength', fontsize=22)
+    fig1.savefig("bar_strength.pdf") 
+
+    fig2.supxlabel('Radius [kpc]', fontsize=22)
+    fig2.supylabel('Bar Strength', fontsize=22)
+    fig2.savefig("bar_histogram.pdf") 
+
+
+if __name__ == '__main__':
+    main()
---
+++ ResearchAssignments/ResearchAssignment7/CenterOfMass2.py
@@ -0,0 +1,327 @@
+
+# Homework 4
+# Center of Mass Position and Velocity
+# Solutions: G.Besla, R. Li, H. Foote
+
+
+# remember this is just a template, you don't need to follow every step
+# if you have your own method to solve the homework, it is totally fine
+
+
+
+# import modules
+import numpy as np
+from numpy.typing import NDArray
+import astropy.units as u
+import astropy.table as tbl
+from ReadFile import Read
+
+class CenterOfMass:
+# Class to define COM position and velocity properties of a given galaxy 
+# and simulation snapshot
+
+    def __init__(self, filename:str, ptype:int=2) -> None:
+        ''' Class to calculate the 6-D phase-space position of a galaxy's center of mass using
+        a specified particle type. 
+            
+            PARAMETERS
+            ----------
+            filename : `str`
+                snapshot file
+            ptype : `int; 1, 2, or 3`
+                particle type to use for COM calculations
+        '''
+     
+        # read data in the given file using Read
+        self.time: NDArray[np.float64]
+        self.total: NDArray[np.float64]
+        self.data: NDArray[np.void]
+        self.time,self.total,self.data = Read(filename)
+
+        # create an array to store indexes of particles of desired Ptype
+        if ptype == 4:
+            self.index:tuple[NDArray[np.int_],...] = np.where(self.data['type'] > 1)
+        else:
+            self.index:tuple[NDArray[np.int_],...] = np.where(self.data['type'] == ptype)
+
+        # store the mass, positions, velocities of only the particles of the given type
+        # the following only gives the example of storing the mass
+        self.m:NDArray[np.float64] = self.data['m'][self.index]
+        # write your own code to complete this for positions and velocities
+        self.x:NDArray[np.float64] = self.data['x'][self.index]
+        self.y:NDArray[np.float64] = self.data['y'][self.index]
+        self.z:NDArray[np.float64] = self.data['z'][self.index]
+        self.vx:NDArray[np.float64] = self.data['vx'][self.index]
+        self.vy:NDArray[np.float64] = self.data['vy'][self.index]
+        self.vz:NDArray[np.float64] = self.data['vz'][self.index]
+
+    def COMdefine(
+        self,
+        a:float|NDArray[np.float64]|u.Quantity,
+        b:float|NDArray[np.float64]|u.Quantity,
+        c:float|NDArray[np.float64]|u.Quantity,
+        m:float|NDArray[np.float64]|u.Quantity
+    ) -> tuple[np.float64,...]:
+
+        ''' Method to compute the COM of a generic vector quantity by direct weighted averaging.
+        
+        PARAMETERS
+        ----------
+        a : `float or np.ndarray of floats`
+            first vector component
+        b : `float or np.ndarray of floats`
+            second vector component
+        c : `float or np.ndarray of floats`
+            third vector component
+        m : `float or np.ndarray of floats`
+            particle masses
+        
+        RETURNS
+        -------
+        a_com : `float`
+            first component on the COM vector
+        b_com : `float`
+            second component on the COM vector
+        c_com : `float`
+            third component on the COM vector
+        '''
+        
+        # write your own code to compute the generic COM
+
+        # Converts floats to numpy arrays. Does not affect numpy array inputs
+        m,a,b,c = np.asarray(m),np.asarray(a),np.asarray(b),np.asarray(c)
+        
+        #using Eq. 1 in the homework instructions
+        # xcomponent Center of mass
+        a_com:np.float64 = np.sum(m*a)/np.sum(m)
+        # ycomponent Center of mass
+        b_com:np.float64 = np.sum(m*b)/np.sum(m)
+        # zcomponent Center of mass
+        c_com:np.float64 = np.sum(m*c)/np.sum(m)
+        
+        # return the 3 components separately
+        return a_com,b_com,c_com
+
+    def COM_P(self,delta:float,volDec:float=2.0) -> u.Quantity:
+        '''
+        Method to compute the position of the center of mass of the galaxy 
+        using the shrinking-sphere method.
+
+        PARAMETERS
+        ----------
+        delta : `float, optional`
+            error tolerance in kpc. Default is 0.1 kpc
+        
+        RETURNS
+        ----------
+        p_COM : `np.ndarray of astropy.Quantity'
+            3-D position of the center of mass in kpc
+        '''
+
+        # Center of Mass Position
+        ###########################
+
+        # Try a first guess at the COM position by calling COMdefine
+        x_COM:np.float64
+        y_COM:np.float64
+        z_COM:np.float64
+        x_COM, y_COM, z_COM = self.COMdefine(self.x, self.y, self.z, self.m)
+        # compute the magnitude of the COM position vector.
+        # write your own code below
+        r_COM:np.float64 = np.linalg.norm((x_COM,y_COM,z_COM))
+
+        
+        # iterative process to determine the center of mass
+
+        # change reference frame to COM frame
+        # compute the difference between particle coordinates
+        # and the first guess at COM position
+        # write your own code below
+        x_new:NDArray[np.float64] = self.x-x_COM
+        y_new:NDArray[np.float64] = self.y-y_COM
+        z_new:NDArray[np.float64] = self.z-z_COM
+        r_new:NDArray[np.float64] = np.linalg.norm(np.vstack((x_new,y_new,z_new)),axis=0)
+
+        # find the max 3D distance of all particles from the guessed COM
+        # will re-start at half that radius (reduced radius)
+        r_max:np.float64 = np.max(r_new)/volDec
+        
+        # pick an initial value for the change in COM position
+        # between the first guess above and the new one computed from half that volume
+        # it should be larger than the input tolerance (delta) initially
+        change:float = 1000.0
+
+        # start iterative process to determine center of mass position
+        # delta is the tolerance for the difference in the old COM and the new one.    
+        
+        while (change > delta):
+            # select all particles within the reduced radius (starting from original x,y,z, m)
+            # write your own code below (hints, use np.where)
+            index2:tuple[NDArray[np.int_],...] = np.where(r_new <= r_max)
+            x2:NDArray[np.float64] = self.x[index2]
+            y2:NDArray[np.float64] = self.y[index2]
+            z2:NDArray[np.float64] = self.z[index2]
+            m2:NDArray[np.float64] = self.m[index2]
+
+            # Refined COM position:
+            # compute the center of mass position using
+            # the particles in the reduced radius
+            # write your own code below
+            x_COM2:np.float64
+            y_COM2:np.float64
+            z_COM2:np.float64
+            x_COM2, y_COM2, z_COM2 = self.COMdefine(x2,y2,z2,m2)
+            # compute the new 3D COM position
+            # write your own code below
+            r_COM2:np.float64 = np.linalg.norm((x_COM2, y_COM2, z_COM2))
+
+            # determine the difference between the previous center of mass position
+            # and the new one.
+            change = np.abs(r_COM-r_COM2)
+
+            # reduce the volume by a factor of 2 again
+            r_max /= volDec
+
+            # Change the frame of reference to the newly computed COM.
+            # subtract the new COM
+            # write your own code below
+            x_new = self.x-x_COM2
+            y_new = self.y-y_COM2
+            z_new = self.z-z_COM2
+            r_new = np.linalg.norm(np.vstack((x_new,y_new,z_new)),axis=0) 
+
+            # set the center of mass positions to the refined values
+            x_COM,y_COM,z_COM,r_COM = x_COM2,y_COM2,z_COM2,r_COM2
+
+        # set the correct units using astropy and round all values
+        # and then return the COM positon vector
+        # write your own code below
+
+        p_COM_arr:NDArray[np.float64] = np.array([x_COM, y_COM, z_COM])
+        p_COM:u.Quantity = np.round(p_COM_arr,2)*u.kpc
+        return p_COM
+    
+        
+    def COM_V(self,x_COM:u.Quantity,y_COM:u.Quantity,z_COM:u.Quantity) -> u.Quantity:
+        ''' Method to compute the center of mass velocity based on the center of mass
+        position.
+
+        PARAMETERS
+        ----------
+        x_COM : 'astropy quantity'
+            The x component of the center of mass in kpc
+        y_COM : 'astropy quantity'
+            The y component of the center of mass in kpc
+        z_COM : 'astropy quantity'
+            The z component of the center of mass in kpc
+            
+        RETURNS
+        -------
+        v_COM : `np.ndarray of astropy.Quantity'
+            3-D velocity of the center of mass in km/s
+        '''
+        
+        # the max distance from the center that we will use to determine 
+        #the center of mass velocity                   
+        rv_max:u.Quantity = 15.0*u.kpc
+
+        # determine the position of all particles relative to the center
+        #     of mass position (x_COM, y_COM, z_COM)
+        # write your own code below
+        # Note that x_COM, y_COM, z_COM are astropy quantities and you
+        #     can only subtract one astropy quantity from another
+        # So, when determining the relative positions, assign the appropriate units to self.x
+        xV:u.Quantity = self.x*u.kpc-x_COM
+        yV:u.Quantity = self.y*u.kpc-y_COM
+        zV:u.Quantity = self.z*u.kpc-z_COM
+        rV:u.Quantity = np.linalg.norm(np.vstack((xV,yV,zV)),axis=0)
+        
+        # determine the index for those particles within the max radius
+        # write your own code below
+        indexV:tuple[NDArray[np.int_],...] = np.where(rV < rv_max)
+        
+        # determine the velocity and mass of those particles within the mas radius
+        # write your own code below
+        vx_new:NDArray[np.float64] = self.vx[indexV]
+        vy_new:NDArray[np.float64] = self.vy[indexV]
+        vz_new:NDArray[np.float64] = self.vz[indexV]
+        m_new:NDArray[np.float64] = self.m[indexV]
+        
+        # compute the center of mass velocity using those particles
+        # write your own code below
+        vx_COM:np.float64
+        vy_COM:np.float64
+        vz_COM:np.float64
+        vx_COM,vy_COM,vz_COM = self.COMdefine(vx_new,vy_new,vz_new,m_new)
+        
+        # create an array to store the COM velocity
+        # write your own code below
+        v_COM_arr:NDArray[np.float64] = np.array([vx_COM, vy_COM, vz_COM])
+
+        # return the COM vector
+        # set the correct units usint astropy
+        # round all values
+        v_COM:u.Quantity = np.round(v_COM_arr,2)*u.km/u.s
+        return v_COM
+
+        
+# ANSWERING QUESTIONS
+#######################
+  
+def main() -> None:
+
+    # Create a Center of mass object for the MW, M31 and M33
+    # below is an example of using the class for MW
+    MW_COM:CenterOfMass = CenterOfMass("MW_000.txt", 2)
+
+    print('\nQUESTION 1\n')
+    
+    # MW:   store the position and velocity COM
+    MW_COM_p:u.Quantity = MW_COM.COM_P(0.1)
+    MW_COM_v:u.Quantity = MW_COM.COM_V(MW_COM_p[0], MW_COM_p[1], MW_COM_p[2])
+    print(f"The Milky Way has a center-of-mass position vector of {MW_COM_p}")
+    print(f"and a velocity vector of {MW_COM_v}")
+
+    # now write your own code to answer questions
+
+    # Now finding the position and velocity vectors for M31...
+    M31_COM:CenterOfMass = CenterOfMass("M31_000.txt", 2)
+    M31_COM_p:u.Quantity = M31_COM.COM_P(0.1)
+    M31_COM_v:u.Quantity = M31_COM.COM_V(M31_COM_p[0], M31_COM_p[1], M31_COM_p[2])
+    print(f"\nM31 has a center-of-mass position vector of {M31_COM_p}")
+    print(f"and a velocity vector of {M31_COM_v}")
+
+    # And M33
+    M33_COM:CenterOfMass = CenterOfMass("M33_000.txt", 2)
+    M33_COM_p:u.Quantity = M33_COM.COM_P(0.1)
+    M33_COM_v:u.Quantity = M33_COM.COM_V(M33_COM_p[0], M33_COM_p[1], M33_COM_p[2])
+    print(f"\nM33 has a center-of-mass position vector of {M33_COM_p}")
+    print(f"and a velocity vector of {M33_COM_v}")
+
+    print('\nQUESTION 2\n')
+    
+    delta_r_MW:u.Quantity = np.linalg.norm(MW_COM_p-M31_COM_p)
+    print(f"The separation between MW and M31 is \n{delta_r_MW:.3f}")
+
+    delta_v_MW:u.Quantity = np.linalg.norm(MW_COM_v-M31_COM_v)
+    print(f"The relative velocity of MW with respect to M31 is \n{delta_v_MW:.3f}")
+
+    print('\nQUESTION 3\n')
+    
+    delta_r_M33:u.Quantity = np.linalg.norm(M33_COM_p-M31_COM_p)
+    print(f"The separation between M33 and M31 is \n{delta_r_M33:.3f}")
+
+    delta_v_M33:u.Quantity = np.linalg.norm(M33_COM_v-M31_COM_v)
+    print(f"The relative velocity of M33 with respect to M31 is \n{delta_v_M33:.3f}")
+
+    print('\nQUESTION 4\n')
+
+    print(
+        '''Using an iterative process to find the center of mass will be critical when
+        stars between the Milky Way and Andromeda start mixing. Gravitational interactions
+        will greatly increase the distance of some stars from the center, creating outliers
+        that will skew the data, and an iterative method will remove them from the dataset
+        ''')
+    
+if __name__ == '__main__' : 
+    main()
